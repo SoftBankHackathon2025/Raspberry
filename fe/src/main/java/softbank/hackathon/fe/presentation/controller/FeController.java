eaf979266990f4b8503b4c773c9555e9a500f417
logan <2220110150@office.kopo.ac.kr>
2025-11-08 23:55:07 +0900

    fix: frontend controller test

--- fe/src/main/java/softbank/hackathon/fe/presentation/controller/FeController.java
+++ fe/src/main/java/softbank/hackathon/fe/presentation/controller/FeController.java
@@ -7,4 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class FeController {
+
+    @GetMapping("/")
+    public String home() {
+        log.info("루트 페이지 요청 받음");
+        return "index"; // templates/index.html을 반환
+    }
+    
 }
